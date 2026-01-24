2a24e18c5fafccf018548250869913ef452dfda9
shivanshcodex92 <codexshivansh@gmail.com>
2026-01-24 18:27:15 +0530

    app.js

--- app.js
+++ app.js
@@ -3,17 +3,23 @@ import {
   getAuth,
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
-  onAuthStateChanged,
-  signOut
+  signOut,
+  onAuthStateChanged
 } from "https://www.gstatic.com/firebasejs/12.8.0/firebase-auth.js";
-
 import {
   getFirestore,
-  doc, setDoc, getDoc,
-  collection, query, where,
-  addDoc, onSnapshot, serverTimestamp
+  doc,
+  setDoc,
+  getDoc,
+  addDoc,
+  collection,
+  onSnapshot,
+  query,
+  where,
+  serverTimestamp
 } from "https://www.gstatic.com/firebasejs/12.8.0/firebase-firestore.js";
 
+/* 🔑 YOUR FIREBASE CONFIG */
 const firebaseConfig = {
   apiKey: "AIzaSyDS6wdYNG2Q7ZUNPjUXdOn-Sqb3cLC4NgQ",
   authDomain: "shivanshcodex-5fa03.firebaseapp.com",
@@ -24,69 +30,65 @@ const app = initializeApp(firebaseConfig);
 const auth = getAuth();
 const db = getFirestore();
 
-let currentChatId = null;
-let currentUser = null;
+let currentUser, currentChatId;
 
 /* AUTH */
-loginBtn.onclick = () =>
-  signInWithEmailAndPassword(auth,email.value,password.value);
+window.register = async () => {
+  const email = email.value;
+  const pass = password.value;
+  const uname = username.value;
 
-registerBtn.onclick = async () => {
-  const res = await createUserWithEmailAndPassword(auth,email.value,password.value);
-  await setDoc(doc(db,"users",res.user.uid),{
-    username:username.value,
-    uid:res.user.uid
-  });
+  const res = await createUserWithEmailAndPassword(auth, email, pass);
+  await setDoc(doc(db, "users", res.user.uid), { username: uname });
+};
+
+window.login = async () => {
+  await signInWithEmailAndPassword(auth, email.value, password.value);
 };
 
 logoutBtn.onclick = () => signOut(auth);
 
-/* AUTH STATE */
-onAuthStateChanged(auth, user => {
-  if(user){
-    currentUser = user;
-    auth.classList.add("hidden");
-    chatApp.classList.remove("hidden");
-  }
+onAuthStateChanged(auth, async user => {
+  if (!user) return;
+  currentUser = user;
+  loginBox.classList.add("hidden");
+  chatApp.classList.remove("hidden");
 });
 
-/* ADD USER / START CHAT */
-addUserBtn.onclick = async () => {
-  const q = query(collection(db,"users"),where("username","==",searchUser.value));
-  onSnapshot(q,snap=>{
-    snap.forEach(async u=>{
-      const chatRef = await addDoc(collection(db,"chats"),{
-        users:[currentUser.uid,u.id]
-      });
-      loadChat(chatRef.id,u.data().username);
+/* CHAT */
+window.startChat = async () => {
+  const q = query(collection(db, "users"), where("username", "==", searchUser.value));
+  const snap = await getDocs(q);
+  snap.forEach(async u => {
+    const chatRef = await addDoc(collection(db, "chats"), {
+      members: [currentUser.uid, u.id],
+      created: serverTimestamp()
     });
+    loadChat(chatRef.id);
   });
 };
 
-/* LOAD CHAT */
-function loadChat(id,name){
-  currentChatId=id;
-  chatHeader.innerText=name;
-  messages.innerHTML="";
-  onSnapshot(collection(db,"chats",id,"messages"),snap=>{
-    messages.innerHTML="";
-    snap.forEach(m=>{
-      const d=m.data();
-      const div=document.createElement("div");
-      div.className="msg "+(d.sender===currentUser.uid?"me":"other");
-      div.innerText=d.text;
+function loadChat(id) {
+  currentChatId = id;
+  chatHeader.innerText = "Chat";
+  onSnapshot(collection(db, "chats", id, "messages"), snap => {
+    messages.innerHTML = "";
+    snap.forEach(m => {
+      const d = m.data();
+      const div = document.createElement("div");
+      div.className = "msg " + (d.sender === currentUser.uid ? "me" : "other");
+      div.innerText = d.text;
       messages.appendChild(div);
     });
   });
 }
 
-/* SEND MESSAGE */
-sendBtn.onclick = async () => {
-  if(!currentChatId) return;
-  await addDoc(collection(db,"chats",currentChatId,"messages"),{
-    text:messageInput.value,
-    sender:currentUser.uid,
-    createdAt:serverTimestamp()
+window.sendMessage = async () => {
+  if (!currentChatId) return;
+  await addDoc(collection(db, "chats", currentChatId, "messages"), {
+    text: msgInput.value,
+    sender: currentUser.uid,
+    time: serverTimestamp()
   });
-  messageInput.value="";
+  msgInput.value = "";
 };
