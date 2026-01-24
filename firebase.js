928274d9d030c4cb93d5c0fc5172b32706dd8ce2
shivanshcodex92 <codexshivansh@gmail.com>
2026-01-24 18:55:10 +0530

    firebase.js

---
+++ firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.8.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.8.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.8.0/firebase-firestore.js";
+
+export const app = initializeApp({
+  apiKey: "AIzaSyDS6wdYNG2Q7ZUNPjUXdOn-Sqb3cLC4NgQ",
+  authDomain: "shivanshcodex-5fa03.firebaseapp.com",
+  projectId: "shivanshcodex-5fa03",
+  storageBucket: "shivanshcodex-5fa03.appspot.com",
+  messagingSenderId: "160989825267",
+  appId: "1:160989825267:web:891e3ac888a46df3920f86"
+});
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
