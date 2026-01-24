c88c2b92506b616ed096235b802f22378bf535fb
shivanshcodex92 <codexshivansh@gmail.com>
2026-01-24 17:16:56 +0530

    page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,5 @@
-"use client";
-
-import { useEffect } from "react";
-import { watchAuth } from "@/lib/auth";
-import { useRouter } from "next/navigation";
+import { redirect } from "next/navigation";
 
 export default function Home() {
-  const router = useRouter();
-
-  useEffect(() => {
-    const unsub = watchAuth((u) => {
-      if (u) router.replace("/app");
-      else router.replace("/login");
-    });
-    return () => unsub();
-  }, [router]);
-
-  return null;
+  redirect("/login");
 }
