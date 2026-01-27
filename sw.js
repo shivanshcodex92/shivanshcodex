66e3572655dc14c3447ce32f160effc0db4ed6cd
shivanshcodex92 <codexshivansh@gmail.com>
2026-01-27 15:51:00 +0530

    sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,5 @@
-const CACHE = "shivanshcodex-v7";
+const CACHE = "shivanshcodex-v8"; // ✅ change this when you update files
+
 const ASSETS = [
   "./",
   "./index.html",
@@ -11,16 +12,18 @@ const ASSETS = [
 
 self.addEventListener("install", (e) => {
   e.waitUntil(
-    caches.open(CACHE).then((c) => c.addAll(ASSETS))
+    caches.open(CACHE).then((cache) => cache.addAll(ASSETS))
   );
+  self.skipWaiting();
 });
 
 self.addEventListener("activate", (e) => {
   e.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(keys.map((k) => (k !== CACHE ? caches.delete(k) : null)))
+    caches.keys().then(keys =>
+      Promise.all(keys.map(k => (k !== CACHE ? caches.delete(k) : null)))
     )
   );
+  self.clients.claim();
 });
 
 self.addEventListener("fetch", (e) => {
